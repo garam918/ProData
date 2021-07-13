bcb7d9b168092d18705b41a400d6d82dbe6c41ca
garam918 <62510764+garam918@users.noreply.github.com>
2021-07-13 22:42:41 +0900

    Feat : Naver Open API에서 데이터를 받아오는 Method 구현

--- app/src/main/java/com/garam/professionalData/network/NetworkService.kt
+++ app/src/main/java/com/garam/professionalData/network/NetworkService.kt
@@ -1,5 +1,7 @@
 package com.garam.professionalData.network
 
+import com.garam.professionalData.data.ResponseData
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
@@ -8,8 +10,8 @@ interface NetworkService {
 
     @GET("/v1/search/doc.json")
     fun search(
-        @Header("X-Naver-Client-Id") clientId : String,
-        @Header("X-Naver-Client-Secret") clientSecret : String,
+        @Header ("X-Naver-Client-Id") clientId: String,
+        @Header ("X-Naver-Client-Secret") clientSecret: String,
         @Query("query") query : String
-        )
+    ) : Call<ResponseData>
 }
