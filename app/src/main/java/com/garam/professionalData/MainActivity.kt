da5b4bb68d12d24e4e486266bffcede663e8116b
garam918 <62510764+garam918@users.noreply.github.com>
2021-07-16 16:36:52 +0900

    Feat : 메인 액티비티와 layout에 databinding 적용

--- app/src/main/java/com/garam/professionalData/MainActivity.kt
+++ app/src/main/java/com/garam/professionalData/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.garam.professionalData
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.garam.professionalData.databinding.ActivityMainBinding
+import com.garam.professionalData.viewModel.ProDataViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var proDataViewModel: ProDataViewModel
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        proDataViewModel = ViewModelProvider(this).get(ProDataViewModel::class.java)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = proDataViewModel
+
+        proDataViewModel.search()
+
     }
 }
